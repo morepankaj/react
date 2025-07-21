95098bb407f707c3a37a6d3015c9d9d031198174
morepankaj <pankajmore1991@gmail.com>
2025-07-21 23:43:02 +0530

    ep 3

--- App.js
+++ App.js
@@ -1,26 +1,19 @@
-/*
-<div id=parent>
-    <div id=child1>
-        <h1>I'm h1 tag</h1>
-        <h2>I'm h2 tag</h2>
-    </div>
-    <div id=child2>
-        <h1>I'm h1 tag</h1>
-        <h2>I'm h2 tag</h2>
-    </div>
-</div>
-ReactElement (Object) => html (browser knows)
-*/
-const parent = React.createElement("h1", { id: "heading", xyz: "abc" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "I'm h1 tag"),
-    React.createElement("h2", {}, "I'm h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I'm h1 tag"),
-    React.createElement("h2", {}, "I'm h2 tag"),
-  ]),
-]);
-console.log(parent);
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+//react.createElement => obj => htmlElement(render)
+
+const parent = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello World, React"
+);
+//console.log(parent);
+
+//jsx => react.createElemnt => obj => htmlElement(render)
+
+const jsxHeading = <h1 id="heading">Hello World, React</h1>;
+//console.log(jsxHeading);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(jsxHeading);
