59b27012b36f6361f74d8941af3f49aa48fd29fd
morepankaj <pankajmore1991@gmail.com>
2025-07-22 00:43:00 +0530

    ep 3

--- App.js
+++ App.js
@@ -1,19 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//react.createElement => obj => htmlElement(render)
-
-const parent = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello World, React"
-);
-//console.log(parent);
-
 //jsx => react.createElemnt => obj => htmlElement(render)
 
-const jsxHeading = <h1 id="heading">Hello World, React</h1>;
+const Title = () => (
+  <h1 className="head" tabIndex="5">
+    Hello World, React...!!
+  </h1>
+);
+
+const num = 1000;
+
+//const jsxHeading = <h1>Hello World, React</h1>;
+
+const HeadingComponent = () => (
+  <div id="container">
+    {num}
+    <Title />
+    <h1 id="heading">Hello World, React</h1>
+  </div>
+);
+//even Title can be call as {Title()} || <Title /> || <Title></Title>
 //console.log(jsxHeading);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading);
+root.render(<HeadingComponent />);
