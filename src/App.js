f2c4e1d474b06d4ab26f2b76e70a0c9538d270f5
morepankaj <pankajmore1991@gmail.com>
2025-08-06 21:31:39 +0530

    routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router";
 
 //jsx => react.createElemnt => obj => htmlElement(render)
 
@@ -28,9 +32,31 @@ import Body from "./components/Body";
 const AppLayout = () => (
   <div className="app">
     <Header />
-    <Body />
+    <Outlet />
   </div>
 );
 
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
+    errorElement: <Error />,
+  },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout />);
+root.render(<RouterProvider router={appRouter} />);
---
+++ src/components/Contact.js
@@ -0,0 +1,10 @@
+const Contact = () => {
+  return (
+    <div>
+      <h1>Contact Us Page</h1>
+      <p>This is a simple contact us page.</p>
+    </div>
+  );
+};
+
+export default Contact;
