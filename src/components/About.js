3ada1c19b13036491966ba484b3cac696ce190f5
morepankaj <pankajmore1991@gmail.com>
2025-08-09 02:15:51 +0530

    class component parent-child rendering

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,33 @@
 import User from "./User";
 import UserClass from "./UserClass";
-const About = () => {
+import React from "react";
+
+class About extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log("parent component props:", props);
+  }
+
+  componentDidMount() {
+    console.log("parent component mounted");
+  }
+
+  render() {
+    console.log("parent component render");
+    return (
+      <div>
+        <h1>About Us Page</h1>
+        <p>This is a simple about us page.</p>
+        <User name="Pankaj More (fn based component)" location="mumbai" />
+        <UserClass name="First" location="worli,Mumbai" />
+        <UserClass name="Second" location="worli,Mumbai" />
+        <UserClass name="third" location="worli,Mumbai" />
+      </div>
+    );
+  }
+}
+
+/* const About = () => {
   return (
     <div>
       <h1>About Us Page</h1>
@@ -12,6 +39,6 @@ const About = () => {
       />
     </div>
   );
-};
+}; */
 
 export default About;
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -3,19 +3,32 @@ class UserClass extends React.Component {
   constructor(props) {
     super(props);
     console.log(props);
+    console.log(this.props.name + " component constructor");
 
     this.state = {
       count: 0,
       count2: 0,
     };
   }
+
+  componentDidMount() {
+    console.log(this.props.name + " componentDidMount");
+  }
+
   render() {
     const { location } = this.props;
+    console.log(this.props.name + " component render");
 
     return (
       <div className="user-card">
         <h2>Count: {this.state.count}</h2>
-        <h2>Count2: {this.state.count2}</h2>
+        <button
+          onClick={() => {
+            this.setState({ count: this.state.count + 1 });
+          }}
+        >
+          Increment count
+        </button>
         <h1>Name: {this.props.name}</h1>
         <h2>Location : {location}</h2>
         <h3>Contact: @pankaj.more</h3>
